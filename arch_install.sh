5cd487ac9e1fa86d5cc43c576ac90b5b770ec5f7
Radiation <60315805+royalzeroo@users.noreply.github.com>
2021-08-09 00:41:50 +0530

    Update arch_install.sh

--- arch_install.sh
+++ arch_install.sh
@@ -54,7 +54,7 @@ mkinitcpio -p linux
 passwd
 # echo root:password | chpasswd
 # pacman --noconfirm -S grub efibootmgr os-prober
-pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call  virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
+pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call  virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font tlp
 
 echo "Enter EFI partition: " 
 read efipartition
@@ -71,6 +71,7 @@ systemctl enable cups.service
 systemctl enable sshd
 systemctl enable avahi-daemon
 systemctl enable reflector.timer
+systemctl enable tlp 
 # systemctl enable libvirtd
 systemctl enable firewalld
 systemctl enable acpid
